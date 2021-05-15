57a2e040576acd00b6856fb42885735d2b997cc1
ashab272000 <muhammedashab@gmail.com>
2021-05-15 16:51:24 +0400

    Created Header
    Added Header.js, Header.css, firebase.js
    Added material-ui/core and material-ui/icons

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCnOerLK-A5xFlBIvMFfUXbOYaC3n46_fI",
+    authDomain: "c-e65e3.firebaseapp.com",
+    projectId: "c-e65e3",
+    storageBucket: "c-e65e3.appspot.com",
+    messagingSenderId: "830102855704",
+    appId: "1:830102855704:web:c127c4078a71908b11236d",
+    measurementId: "G-HDYSR6L70S"
+  };
